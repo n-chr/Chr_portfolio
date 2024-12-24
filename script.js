439127d84e128c62ad43342610e3c961cce1c5ad
n-chr <131326994+n-chr@users.noreply.github.com>
2024-12-24 09:19:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// JavaScript
+
+const items = document.querySelectorAll('.img-item');
+
+// console.log(items);
+// console.log(items.length);
+
+// for文
+for(let i = 0; i < items.length; i++){
+
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['0 50px', 0],
+    filter: ['blur(10px)', 'blur(0)'],
+  };
+  
+  const options = {
+    duration: 600,
+    delay: i * 400,
+    fill: 'forwards',
+  };
+
+  items[i].animate(keyframes, options);
+}
+
+// forEach文
+// items.forEach((item, i) => {
+
+//   const keyframes = {
+//     opacity: [0, 1],
+//     translate: ['0 50px', 0],
+//     filter: ['blur(10px)', 'blur(0)'],
+//   };
+  
+//   const options = {
+//     duration: 600,
+//     delay: i * 400,
+//     fill: 'forwards',
+//   };
+
+//   item.animate(keyframes, options);
+// });
+
+// フェード
+const fade = document.querySelector('.fade');
+
+const fadeKeyframes = {
+  opacity: [0, 1],
+}
+
+const fadeOptions = {
+  duration: 3000,
+  easing: 'ease',
+  fill: 'forwards',
+}
+
+fade.animate(fadeKeyframes, fadeOptions);
